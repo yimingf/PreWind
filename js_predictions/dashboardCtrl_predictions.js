1ed9c1ad20f83fb4a92b3c2ed8a0053d1d701cc4
Deepika Dipesh <dipika_vinaykiya@yahoo.com>
2017-03-11 18:57:57 +0100

    Added wind prediction controller js

--- js_predictions/dashboardCtrl_predictions.js
+++ js_predictions/dashboardCtrl_predictions.js
@@ -20,13 +20,14 @@ dashboardApp.controller('DashboardCtrlForPredictions', function ($location, $sco
      $scope.nameOfWindfarm = $location.search().wf;
      }*/
 
-    $scope.setCurrentWindfarmId = function (windfarmId) {
+    $scope.setCurrentWindfarmId= function (windfarmId) {
         windfarm = $scope.nameOfWindfarms.indexOf(windfarmId)+1;
+        console.log(windfarm);
     };
 
 
     //Get all data from API
-    $scope.buildDataFromApi = function () {
+   // $scope.buildDataFromApi = function () {
         $scope.apiCallStatus = "Loading...";
         $scope.resultCounter = 0;
         $scope.dailyProduction = undefined;
@@ -37,13 +38,10 @@ dashboardApp.controller('DashboardCtrlForPredictions', function ($location, $sco
         promiseDailyData.then(
             function (response) {
                 $scope.dailyProduction = response.data;
-                //console.log($scope.dailyProduction);
-                count++;
-                console.log(count);
-                if (count==2){
+                console.log($scope.dailyProduction);
+                $scope.buildDataFromResults();
+                $scope.buildAllGraphs();
 
-                    $scope.buildDataFromResults();
-                }
             },
             function (response) {
                 $scope.dailyProduction = undefined;
@@ -51,28 +49,7 @@ dashboardApp.controller('DashboardCtrlForPredictions', function ($location, $sco
                 alert("Error: " + JSON.stringify(response));
             });
 
-        //getting speed data from API
-        var promiseDailySpeedData = $scope.dataService.getDailyWindspeedDataForPredictions(wp1);
-        promiseDailySpeedData.then(
-            function (response) {
-                $scope.dailySpeedData = response.data;
-                console.log($scope.dailySpeedData);
-                count++;
-                console.log(count);
-                if (count==2){
-
-                    $scope.buildDataFromResults();
-                }
-            },
-            function (response) {
-                $scope.dailySpeedData = undefined;
-                $scope.apiCallStatus = "Error :(";
-                alert("Error: " + JSON.stringify(response));
-            });
-
-
-
-    };
+   // };
 
 
     //Push the data into specific array
@@ -86,21 +63,44 @@ dashboardApp.controller('DashboardCtrlForPredictions', function ($location, $sco
             }
         });
 
-        $scope.buildAllGraphs();
+        //$scope.buildAllGraphs();
     };
 
+    function getDailyWindspeedDataForWindFarm(wp) {
+        //getting speed data from API
+        var promiseDailySpeedData = $scope.dataService.getDailyWindspeedDataForPredictions(wp);
+        promiseDailySpeedData.then(
+            function (response) {
+                $scope.dailySpeedData = response.data;
+                console.log(wp);
+                if (wp==1){
+                    $scope.drawgraph_wp1();
+                }
+                if (wp==2){
+                    $scope.drawgraph_wp2();
+                }
+                if (wp==3){
+                    $scope.drawgraph_wp3();
+                }
 
-    //Build graph for selected windfarm
-    $scope.buildAllGraphs = function(){
-        $scope.graph_wp1();
-        //  $scope.graph_wp2();
-        //  $scope.graph_wp3();
+            },
+            function (response) {
+                $scope.dailySpeedData = undefined;
+                $scope.apiCallStatus = "Error :(";
+                alert("Error: " + JSON.stringify(response));
+            });
 
-    };
+    }
+
+
+    $scope.buildGraph = function(wp){
+        getDailyWindspeedDataForWindFarm(wp)
+
+    }
 
     //build graph for windfarm WP1
-    $scope.graph_wp1 = function(){
-
+    $scope.drawgraph_wp1 = function(){
+        $scope.dailySpeedData = getDailyWindspeedDataForWindFarm(1);
         var uncertainty_ProductionUpperLimit = [];
         var uncertainty_ProductionLowerLimit = [];
         var uncertainty_ProductionDate = [];
@@ -180,8 +180,8 @@ dashboardApp.controller('DashboardCtrlForPredictions', function ($location, $sco
         };
 
         var trace4 = {
-            x: [14/0, 14/0, 14/0, 14/0, 14/0, 14/0, 14/0, 14/0, 14/0],
-            y: [0, 10, 20, 40, 50, 60, 80, 90, 100],
+            x: ["14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0"],
+            y: [0, 10, 20, 40, 50, 60, 80, 90, 120],
             mode: 'lines',
             name: 'Split',
             line: {
@@ -234,132 +234,274 @@ dashboardApp.controller('DashboardCtrlForPredictions', function ($location, $sco
 
     };
 
-    /* $scope.graph_wp2 = function(){
-
-     var uncertainty_ProductionUpperLimit = [];
-     var uncertainty_ProductionLowerLimit = [];
-     var uncertainty_ProductionDate = [];
-     var wp2_SpeedData = [];
-     var upperLimitValue;
-     var lowerLimitValue;
-     var j=0;
 
 
-     //getting uncertainty production value via forloop
-     var arrayLength_wp2_ProductionValue = wp2_ProductionValue.length;
-     for (var i = 48; i < arrayLength_wp2_ProductionValue; i++) {
-     j++;
-     upperLimitValue = wp2_ProductionValue[i] + 2 + j;
-     lowerLimitValue = wp2_ProductionValue[i] - 2 - j;
-     uncertainty_ProductionUpperLimit.push(upperLimitValue);
-     uncertainty_ProductionLowerLimit.push(lowerLimitValue);
-     }
+    $scope.drawgraph_wp2 = function(){
 
-     //getting uncertainty production hour via forloop
-     var arrayLength_UncertaintyDate = ProductionDate.length;
-     for (var i = 48; i < arrayLength_UncertaintyDate; i++) {
-     uncertainty_ProductionDate.push(ProductionDate[i]);
-     }
+        var uncertainty_ProductionUpperLimit = [];
+        var uncertainty_ProductionLowerLimit = [];
+        var uncertainty_ProductionDate = [];
+        var wp2_SpeedData = [];
+        var upperLimitValue;
+        var lowerLimitValue;
+        var j=0;
 
 
-     $scope.dailySpeedData.forEach(function (element) {
-     wp2_SpeedData.push(element.ws);
-     });
+        //getting uncertainty production value via forloop
+        var arrayLength_wp2_ProductionValue = wp2_ProductionValue.length;
+        for (var i = 48; i< arrayLength_wp2_ProductionValue; i++) {
+            j++;
+            if(i<92){
+                upperLimitValue = 0;
+                lowerLimitValue = 0;
+            }
+            else {
+                upperLimitValue = wp2_ProductionValue[i] + j;
+                lowerLimitValue = wp2_ProductionValue[i] - j;
+            }
+            uncertainty_ProductionUpperLimit.push(upperLimitValue);
+            uncertainty_ProductionLowerLimit.push(lowerLimitValue);
+        }
+
+        //getting uncertainty production hour via forloop
+        var arrayLength_UncertaintyDate = ProductionDate.length;
+        for (var i = 48; i < arrayLength_UncertaintyDate; i++) {
+            uncertainty_ProductionDate.push(ProductionDate[i]);
+        }
+
+      //  scope.dailySpeedData = getDailyWindspeedDataForWindFarm(2);
+        $scope.dailySpeedData.forEach(function (element) {
+            wp2_SpeedData.push(element.ws);
+        });
 
 
-     var trace1 = {
-     x: ProductionDate,
-     y: wp2_ProductionValue,
-     mode: 'lines',
-     name: 'ProductionValue',
-     line: {
-     dash: 'solid',
-     width: 3
-     }
-     };
+        var trace1 = {
+            x: ProductionDate,
+            y: wp2_ProductionValue,
+            mode: 'lines',
+            name: 'ProductionValue',
+            line: {
+                dash: 'solid',
+                width: 3
+            }
+        };
 
-     var trace2 = {
-     x: uncertainty_ProductionDate,
-     y: uncertainty_ProductionUpperLimit,
-     marker: {color: 'rgb(244, 119, 66)'},
-     mode: 'lines',
-     name: 'uncertainty range',
-     line: {
-     dash: 'solid',
-     width: 2
-     }
-     };
+        var trace2 = {
+            x: uncertainty_ProductionDate,
+            y: uncertainty_ProductionUpperLimit,
+            marker: {color: 'rgb(244, 119, 66)'},
+            mode: 'lines',
+            name: 'uncertainty range',
+            line: {
+                dash: 'solid',
+                width: 2
+            }
+        };
 
-     var trace3 = {
-     x: uncertainty_ProductionDate,
-     y: uncertainty_ProductionLowerLimit,
-     marker: {color: 'rgb(244, 119, 66)'},
-     mode: 'lines',
-     name: 'uncertainty range',
-     line: {
-     dash: 'solid',
-     width: 2
-     }
-     };
+        var trace3 = {
+            x: uncertainty_ProductionDate,
+            y: uncertainty_ProductionLowerLimit,
+            marker: {color: 'rgb(244, 119, 66)'},
+            mode: 'lines',
+            name: 'uncertainty range',
+            line: {
+                dash: 'solid',
+                width: 2
+            }
+        };
 
-     var trace4 = {
-     x: [14/0, 14/0, 14/0, 14/0, 14/0, 14/0, 14/0, 14/0, 14/0],
-     y: [0, 10, 20, 40, 50, 60, 80, 90, 100],
-     mode: 'lines',
-     name: 'Split',
-     line: {
-     dash: 'dot',
-     width: 2
-     }
-     };
+        var trace4 = {
+            x: ["14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0"],
+            y: [0, 10, 20, 40, 50, 60, 80, 90, 120],
+            mode: 'lines',
+            name: 'Split',
+            line: {
+                dash: 'dot',
+                width: 2
+            }
+        };
 
-     var trace5 = {
-     x: ProductionDate,
-     y: wp1_SpeedData,
-     type: 'scatter',
-     name: 'windspeed data',
-     line: { // set the width of the line.
-     width: 2,
-     dash: 'dot',
-     },
-     yaxis: 'y2'
-     };
+        var trace5 = {
+            x: ProductionDate,
+            y: wp2_SpeedData,
+            type: 'scatter',
+            name: 'windspeed data',
+            line: { // set the width of the line.
+                width: 2,
+                dash: 'dot',
+            },
+            yaxis: 'y2'
+        };
 
-     var data = [trace1, trace2, trace3, trace4, trace5];
+        var data = [trace1, trace2, trace3, trace4, trace5];
 
-     var layout = {
-     xaxis: {
-     title: 'date(Hour)',
-     autorange: true
-     },
-     yaxis: {
-     title: 'Production Value',
-     range: [0, 120],
-     autorange: false
-     },
-     yaxis2: {
-     title: 'm/s',
-     titlefont: {color: 'rgb(148, 103, 189)'},
-     tickfont: {color: 'rgb(148, 103, 189)'},
-     overlaying: 'y',
-     side: 'right'
-     },
-     legend: {
-     y: 2,
-     traceorder: 'reversed',
-     font: {
-     size: 16
-     }
-     }
-     };
+        var layout = {
+            xaxis: {
+                title: 'date(Hour)',
+                autorange: true
+            },
+            yaxis: {
+                title: 'Production Value',
+                range: [0, 120],
+                autorange: false
+            },
+            yaxis2: {
+                title: 'm/s',
+                titlefont: {color: 'rgb(148, 103, 189)'},
+                tickfont: {color: 'rgb(148, 103, 189)'},
+                overlaying: 'y',
+                side: 'right'
+            },
+            legend: {
+                y: 2,
+                traceorder: 'reversed',
+                font: {
+                    size: 16
+                }
+            }
+        };
 
-     Plotly.newPlot('wind-production-chart', data, layout);
+        Plotly.newPlot('wind-prediction-chart', data, layout);
 
-     };*/
+    };
+
+    $scope.drawgraph_wp3 = function(){
+
+        var uncertainty_ProductionUpperLimit = [];
+        var uncertainty_ProductionLowerLimit = [];
+        var uncertainty_ProductionDate = [];
+        var wp3_SpeedData = [];
+        var upperLimitValue;
+        var lowerLimitValue;
+        var j=0;
+
+
+        //getting uncertainty production value via forloop
+        var arrayLength_wp3_ProductionValue = wp3_ProductionValue.length;
+        for (var i = 48; i< arrayLength_wp3_ProductionValue; i++) {
+            j++;
+            if(i<84){
+                upperLimitValue = 0;
+                lowerLimitValue = 0;
+            }
+            else {
+                upperLimitValue = wp3_ProductionValue[i] + 2 + j;
+                lowerLimitValue = wp3_ProductionValue[i] - 2 - j;
+            }
+            uncertainty_ProductionUpperLimit.push(upperLimitValue);
+            uncertainty_ProductionLowerLimit.push(lowerLimitValue);
+        }
+
+        //getting uncertainty production hour via forloop
+        var arrayLength_UncertaintyDate = ProductionDate.length;
+        for (var i = 48; i < arrayLength_UncertaintyDate; i++) {
+            uncertainty_ProductionDate.push(ProductionDate[i]);
+        }
+
+
+       // scope.dailySpeedData = getDailyWindspeedDataForWindFarm(3);
+        $scope.dailySpeedData.forEach(function (element) {
+            if(element.date != undefined) {
+                if (element.date > 11 && element.date < 16) {
+                    wp3_SpeedData.push(element.ws);
+                }
+            }
+        });
+
+        var trace1 = {
+            x: ProductionDate,
+            y: wp3_ProductionValue,
+            mode: 'lines',
+            name: 'ProductionValue',
+            line: {
+                dash: 'solid',
+                width: 3
+            }
+        };
+
+        var trace2 = {
+            x: uncertainty_ProductionDate,
+            y: uncertainty_ProductionUpperLimit,
+            marker: {color: 'rgb(244, 119, 66)'},
+            mode: 'lines',
+            name: 'uncertainty range',
+            line: {
+                dash: 'solid',
+                width: 2
+            }
+        };
+
+        var trace3 = {
+            x: uncertainty_ProductionDate,
+            y: uncertainty_ProductionLowerLimit,
+            marker: {color: 'rgb(244, 119, 66)'},
+            mode: 'lines',
+            name: 'uncertainty range',
+            line: {
+                dash: 'solid',
+                width: 2
+            }
+        };
+
+        var trace4 = {
+            x: ["14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0","14/0"],
+            y: [0, 10, 20, 40, 50, 60, 80, 90, 120],
+            mode: 'lines',
+            name: 'Split',
+            line: {
+                dash: 'dot',
+                width: 2
+            }
+        };
+
+        var trace5 = {
+            x: ProductionDate,
+            y: wp3_SpeedData,
+            type: 'scatter',
+            name: 'windspeed data',
+            line: { // set the width of the line.
+                width: 2,
+                dash: 'dot',
+            },
+            yaxis: 'y2'
+        };
+
+        var data = [trace1, trace2, trace3, trace4, trace5];
+
+        var layout = {
+            xaxis: {
+                title: 'date(Hour)',
+                autorange: true
+            },
+            yaxis: {
+                title: 'Production Value',
+                range: [0, 120],
+                autorange: false
+            },
+            yaxis2: {
+                title: 'm/s',
+                titlefont: {color: 'rgb(148, 103, 189)'},
+                tickfont: {color: 'rgb(148, 103, 189)'},
+                overlaying: 'y',
+                side: 'right'
+            },
+            legend: {
+                y: 2,
+                traceorder: 'reversed',
+                font: {
+                    size: 16
+                }
+            }
+        };
+
+        Plotly.newPlot('wind-prediction-chart', data, layout);
+
+    };
+
+    $scope.buildGraph(2);
 
 
 
-    $scope.buildDataFromApi();
 
 
 });
