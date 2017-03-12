2952a584d7ba38e81f95d5fc60c7bf9dbba1dd25
Fan Yiming <alkalidic@gmail.com>
2017-03-12 14:28:04 +0100

    fix that

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var dashboardApp = angular.module('dashboard',
   ['ngRoute', 'ngResource', 'ngCookies', 'customFiltersModule'])
-  .config(function ($locationProvider, $routeProvider) {
+  .config(function ($locationProvider) {
     $locationProvider.html5Mode({
       enabled: false,
       requireBase: false
